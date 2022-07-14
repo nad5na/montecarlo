3b0d6e33bc479a47a69063b6eb3b21f2c468389a
nad5na <107503039+nad5na@users.noreply.github.com>
2022-07-14 10:39:07 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='My MC Package',
+      version='0.1',
+      description='Monte Carlo Simulation package with Die(), Game(), and Analyzer() classes in main montecarlo.py file',
+      url='https://github.com/nad5na/montecarlo',
+      author='Noah Dunn',
+      author_email='nad5na@virginia.edu',
+      license='MIT',
+      packages=['mc_package'])
